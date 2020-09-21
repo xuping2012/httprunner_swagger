bf8c7bc8e770ae02b1a0e82f5abb95dde84ac98c
studyWorkSpace <oupoor@qq.com>
2020-09-21 18:18:11 +0800

    支持json/yaml数据格式互转

--- utils/HandleJson.py
+++ utils/HandleJson.py
@@ -3,6 +3,7 @@
 # @File    : processingJson.py
 
 import json
+import yaml
 from utils.HandleLogging import log as logger
 
 
@@ -35,4 +36,34 @@ def write_data(res, json_path):
         logger.info('Interface Params Total：{} ,write to json file successfully!'.format(len(res)))
     else:
         logger.error('\n:{} Params is not dict.\n'.format(write_data.__name__))
-        
+
+ 
+
+def json_to_yaml(json_file):
+    """支持json格式转yaml"""
+    if json_file.endswith("json"):
+        with open(json_file,"r") as pf:
+            json_to_dict = json.loads(pf.read())
+        yaml_file=json_file.replace(".json",".yaml")
+        with open(yaml_file,"w") as fp:
+            yaml.safe_dump(json_to_dict,stream=fp,default_flow_style=False)
+            print("json转yaml成功!!!")
+    else:
+        print("不是json结尾的文件!!!")
+
+def yaml_to_yaml(yaml_file):
+    """支持json格式转yaml"""
+    if yaml_file.endswith("yaml"):
+        with open(yaml_file,"r") as pf:
+            #先将yaml转换为dict格式
+            yaml_to_dict = yaml.load(pf,Loader=yaml.FullLoader)
+            dict_to_json = json.dumps(yaml_to_dict,sort_keys=False,indent=4,separators=(',',': '))
+        json_file=yaml_file.replace(".yaml",".json")
+        with open(json_file,"w") as fp:
+            fp.write(dict_to_json)
+            print("yaml转json成功!!!")
+    else:
+        print("不是yaml结尾的文件!!!")
+
+# json_to_yaml("exam.json")
+# yaml_to_yaml("exam.yaml")
